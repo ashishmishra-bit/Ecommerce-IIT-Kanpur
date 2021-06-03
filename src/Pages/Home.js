727414c80921b486fbff73d50675890d9a9d3f4c
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-06-03 13:38:09 +0530

    Header corrected

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,7 +4,7 @@ import '../css/Home.css';
 const Home = () => {
     return (
         <>
-        <div className="container">            
+        <div className="header">            
         </div>
         <h1>Home</h1>
         <h1>Ashish Kumar mishra</h1>
